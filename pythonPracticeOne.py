9f9195723f3153544fc49fa42403d10393d41d14
haeun <haeunk27@gmail.com>
2023-09-07 20:18:04 -0500

    Update pythonPracticeOne.py

--- pythonPracticeOne.py
+++ pythonPracticeOne.py
@@ -1,7 +1,14 @@
 def sum():
-	num1 = input()
-	num2 = input()
-	num3 = input()
+	num1 = int(input())
+	num2 = int(input())
+	num3 = int(input())
 	return num1+num2+num3
 
+def sayHello():
+	name1 = input()
+	name2 = input()
+	print('Hello ' + name1 + '!')
+	print('Hello ' + name2 + '!')
+
 print(sum())
+sayHello()
